3f69ccebfced06055c8646763995e53f947969dd
Starry0114 <128798573+Starry0114@users.noreply.github.com>
2025-08-08 10:44:52 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,21 +4,23 @@ class Database {
     private $db_name = "demo";
     private $username = "starry";
     private $password = "Vincent020114";
+    private $port = "3306";
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, 
                 $this->username, 
                 $this->password,
                 [
-                    PDO::MYSQL_ATTR_SSL_CA => '/path/to/BaltimoreCyberTrustRoot.crt.pem',
+                    PDO::MYSQL_ATTR_SSL_CA => 'C:\Users\shmcv\Downloads\DigiCertGlobalRootCA.crt.pem', // 替换为实际路径
                     PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true
                 ]
             );
             $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // 添加错误模式设置
         } catch(PDOException $e) {
             echo "数据库连接错误: " . $e->getMessage();
         }
@@ -26,4 +28,3 @@ class Database {
     }
 }
 ?>
-
